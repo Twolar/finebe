abc3c229181e217d5bfb60879c77d9aaee31564c
“Taylor <t4ybennett@gmail.com>
2024-05-26 17:36:01 +0100

    reset yo

--- finebe-api/Src/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace finebe_api.Models;
-
-public class ApplicationUser : IdentityUser<Guid>
-{
-    // Add additional profile data for application users by adding properties to this class
-}
-
